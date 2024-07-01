811fb7bef58714de04ab28f6907fa67a77e1df41
aaditya9803 <aaditya9803@gmail.com>
2024-07-01 22:11:59 +0200

    correction to css

--- public/main.js
+++ public/main.js
@@ -1,63 +1,40 @@
 var socket = io();
-        var messages = document.getElementById('messages');
-        var input = document.getElementById('input');
-        var sendButton = document.getElementById('send');
+var messages = document.getElementById('messages');
+var input = document.getElementById('input');
+var sendButton = document.getElementById('send');
 
+function addMessage(message, sender) {
+    var item = document.createElement('div');
+    item.classList.add('message', sender);
+    item.textContent = message;
+    messages.appendChild(item);
+    messages.scrollTop = messages.scrollHeight;
+}
 
-        function addMessage(message, sender) {
-            var item = document.createElement('div');
-            item.style.marginBottom = "10px";
-            item.textContent = message;
-            item.style.backgroundColor = "#05cee9";
-            item.style.color ="black";
-            item.style.padding ="5px";
-            item.style.borderRadius = "5px";
-            item.style.marginLeft = "345px";
-            item.style.textAlign ="right";
-            item.style.fontSize = "15px"
-            item.classList.add("message",sender);
-            messages.appendChild(item);
-            
-            
-            messages.scrollTop = messages.scrollHeight;
-            
-        }
-        function addMessage1(message, sender) {
-            var item2 = document.createElement('div');
-            
-            item2.textContent = message;
-            item2.style.backgroundColor = "#584fd9";
-            item2.style.marginBottom = "10px";
-            item2.style.width = "350px";
-            item2.style.padding ="5px";
-            item2.style.borderRadius = "5px";
-            item2.classList.add("message",sender);
-            messages.appendChild(item2);
-            messages.scrollTop = messages.scrollHeight;
-            
-        }
+socket.on('chatbot message', function(msg) {
+    addMessage(msg, 'chatbot');
+});
 
+sendButton.onclick = function() {
+    var msg = input.value;
+    if (msg) {
+        addMessage(msg, 'user');
+        socket.emit('user message', msg);
+        input.value = '';
+        adjustTextareaHeight(); // Reset the height after sending
+    }
+};
 
-     
+input.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault(); // Prevent adding a new line
+        sendButton.click();
+    }
+});
 
-        
- 
-        socket.on('chatbot message', function(msg) {
-            addMessage1(msg, 'chatbot');
-        
-        });
+function adjustTextareaHeight() {
+    input.style.height = 'auto';
+    input.style.height = (input.scrollHeight) + 'px';
+}
 
-        sendButton.onclick = function() {
-            var msg = input.value ;
-            if (msg) {
-                addMessage(msg, 'user');
-                socket.emit('user message', msg);
-                input.value = '';
-            }
-        };
-
-        input.addEventListener('keypress', function(event) {
-            if (event.key === 'Enter') {
-                sendButton.click();
-            }
-        });
+input.addEventListener('input', adjustTextareaHeight);

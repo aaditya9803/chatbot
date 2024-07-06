832089eba8188c68b1d6b13cc7d4b59bdbe50308
aaditya9803 <aaditya9803@gmail.com>
2024-07-07 00:44:54 +0200

    using state in in python

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,19 @@
 import sys
 from datetime import datetime
+import json
 
 today = datetime.today()
 # greet1 = ["hello", "hi", "Hey"]
 # greet2 = "What would you like to have? We have "
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-foods = ["pizza", "burger", "pasta"]
 menu = {
     "Monday": ["Pasta Saloniki", "Cauliflower crispy medallion with tomato dip and carrot vegetables", "Colorful vegetable salad", "Mixed salad","Peach curd"],
     "Tuesday": ["Mexican rice dish", "Onion schnitzel with spaetzle","Colorful vegetable salad", "Mixed salad"],
     "Wednesday": ["Indian vegetable curry", "Meatballs with cream sauce and mashed potatoes", "Colorful vegetable salad", "Mixed salad", "Eggnog pudding"],
     "Thursday": ["Spinach with Gorgonzola", "Turkey steak gardener with rice", "Colorful vegetable salad", "Mixed salad", "Yoghurt cream Pineapple-Coconut"],
     "Friday": ["Veganes Spargelrisotto", "Baked fish patties with potato salad", "Mixed salad", "Vanillequark"],
-    "Saturday": ["nothing. We are closed today."],
-    "Sunday": ["nothing. We are closed today."]
+    "Saturday": ["nothing. We are closed."],
+    "Sunday": ["nothing. We are closed."]
 }
 prices = {
     "Pasta Saloniki": ["2,80", "3,40", "4,40"],
@@ -34,41 +34,97 @@ prices = {
     "Vanillequark": ["0,90", "1,10", "1,60"],
 }
 
-def get_chatbot_response(message):
+def get_chatbot_response(message, state):
     message = message.lower()
-    for day in days:
-        if day.lower() in message:
-            return f"On {day}s we have {', '.join(menu[day])}"
+    response = {}
+    # for day in days:
+    #     if day.lower() in message:
+    #         return f"On {day}s we have <ol><li>{'</li><li>'.join(menu[day])}</li></ol>"
 
-    for food in foods:
-        if food in message.lower() and ("price" or "cost" in message.lower()):
-            return f'{food} costs {prices[food]} dollars.'
+    # for food in foods:
+    #     if food in message.lower() and ("price" or "cost" in message.lower()):
+    #         return f'{food} costs {prices[food]} dollars.'
+        
 
-    if "hello" in message or "hi" in message or "hey" in message or "today" in message:
-        return f"Hi, Today we have <ul><li>{'</li><li>'.join(menu[days[today.weekday()]])}</li></ul>"
+    if 'hi' in message.lower() or 'hello' in message.lower() or 'hey' in message.lower() or 'today' in message.lower():
+        response['message'] = "Hi, Would you like to know the menu for today?"
+        response['state'] = {'lastMessage': 'menu_today'}
+
+    elif state['lastMessage'] == 'menu_today':
+        if 'yes' in message.lower() or 'yeah' in message.lower() or 'sure'in message.lower() or 'ok'in message.lower() or 'okay'in message.lower() or 'today' in message.lower():
+            response['message'] = f"Hi, Today we have <ol><li>{'</li><li>'.join(menu[days[today.weekday()]])}</li></ol>"
+            response['state'] = {'lastMessage': 'food_list'}
+        if 'No' in message.lower() or 'no' in message.lower() or 'nope'in message.lower():
+            response['message'] = "Say 'hi' to start again."
+            response['state'] = {'lastMessage': None}
+        else:
+            response['message'] = "I could not understand you. <br> Say 'hi' to start again."
+            response['state'] = {'lastMessage': None}
+
     elif "tomorrow" in message:
         if today.weekday() != 6:
             tomorrow = today.weekday() + 1
         else:
             tomorrow = 0
-        return f"On {days[tomorrow]}s we have <ul><li>{'</li><li>'.join(menu[days[tomorrow]])}</li></ul>"
+        response['message'] = f"On {days[tomorrow]}s we have <ol><li>{'</li><li>'.join(menu[days[tomorrow]])}</li></ol>"
+        response['state'] = {'lastMessage': 'menu_tomorrow'}
+
+    elif state['lastMessage'] == 'menu_tomorrow':
+        if 'order' in message.lower() or 'order food' in message.lower() or 'pack'in message.lower() or 'ok'in message.lower() or 'okay'in message.lower() or 'today' in message.lower():
+            response['message'] = "You can't order food for tomorrow. <br> Please comeback tomorrow to order <br> Thank you!"
+            response['state'] = {'lastMessage': None}
+
+        # else:
+        #     response['message'] = "I could not understand you. <br> Say 'hi' to start again."
+        #     response['state'] = {'lastMessage': None}
+        
     elif "yesterday" in message:
         if today.weekday() != 0:
             yesterday = today.weekday() - 1
         else:
             yesterday = 6
-        return f"On {days[yesterday]}s we have <ul><li>{'</li><li>'.join(menu[days[yesterday]])}</li></ul>"
+        response['message'] = f"On {days[tomorrow]}s we have <ol><li>{'</li><li>'.join(menu[days[yesterday]])}</li></ol>"
+        response['state'] = {'lastMessage': 'menu_yesterday'} 
+
+    # elif state['lastMessage'] == 'food_list':
+    #     if message in prices.keys():
+    #         response['message'] = f'{message} costs {", ".join(prices[message])} euros.'
+    #         response['state'] = {'lastMessage': 'price'}
+    #     else:
+    #         response['message'] = "Sorry, I don't have the price for that. Would you like to know the menu for tomorrow?"
+    #         response['state'] = {'lastMessage': 'menu_tomorrow'}
+
+
+
+
+    # elif "tomorrow" in message:
+    #     if today.weekday() != 6:
+    #         tomorrow = today.weekday() + 1
+    #     else:
+    #         tomorrow = 0
+    #     return f"On {days[tomorrow]}s we have <ol><li>{'</li><li>'.join(menu[days[tomorrow]])}</li></ol>"
+    # elif "yesterday" in message:
+    #     if today.weekday() != 0:
+    #         yesterday = today.weekday() - 1
+    #     else:
+    #         yesterday = 6
+    #     return f"On {days[yesterday]}s we have <ol><li>{'</li><li>'.join(menu[days[yesterday]])}</li></ol>"
 
     elif 'menu' in message.lower() and ('all' or 'whole') in message.lower():
-        return f"Our menu includes: <ul><li>{'</li><li>'.join(list(prices.keys()))}</li></ul>"
+        return f"Our menu includes: <ol><li>{'</li><li>'.join(list(prices.keys()))}</li></ol>"
     elif 'menu' in message.lower() or ('menu' and 'today') in message.lower():
-        return f"Today we have <ul><li>{'</li><li>'.join(menu[days[today.weekday()]])}</li></ul>"
+        return f"Today we have <ol><li>{'</li><li>'.join(menu[days[today.weekday()]])}</li></ol>"
     elif 'hours' in message.lower():
         return 'We are open from 7:30 AM to 3:00 PM on working days.'
+    elif 'thank you' in message.lower() or 'thankyou' in message.lower():
+        return 'You are welcome! Have a nice day! <br> Bye!'
     else:
-        return 'I am sorry, I didn\'t understand that.'
+        response['message'] = "Please say 'hi' to start again."
+        response['state'] = {'lastMessage': None}
+    return response
 
 if __name__ == '__main__':
     message = sys.argv[1]
-    response = get_chatbot_response(message)
-    print(response)
+    state = json.loads(sys.argv[2])
+    response = get_chatbot_response(message, state)
+    print(json.dumps(response))
